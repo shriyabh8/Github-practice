e09eaf6692ad86ee2d4b5466c92a0b87d562e54b
Shriya Bhamidipati <shriya.bhamidipati@gmail.com>
2023-06-12 08:25:24 -0700

    Initial Commit

---
+++ Github practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Github practice
+//
+//  Created by Shriya Bhamidipati on 6/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
